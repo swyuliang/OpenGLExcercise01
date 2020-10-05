87146163d1d3497db90304caa63408b1c7e6b79d
swyuliang <36142847+swyuliang@users.noreply.github.com>
2020-10-05 23:28:32 +0800

    swyuliang
    hello triangles

--- main.cpp
+++ main.cpp
@@ -6,6 +6,33 @@
 #include<GLFW\glfw3.h>
 
 
+float vertices[] = {
+	-0.5f,-0.5f,0.0f,
+	0.5f,-0.5f,0.0f,
+	0.0f,0.5f,0.0f,
+	//0.5f,-0.5f,0.0f,
+	//0.0f,0.5f,0.0f,
+	0.8f,0.8f,0.0f
+	//-0.5f,0.5f,0.0f
+};
+
+unsigned int indices[] ={
+ 0,1,2,
+ 2,1,3
+};
+
+const char* vertexShaderSource =
+	"#version 330 core \n"
+	"layout(location = 0) in vec3 aPos;\n"
+	"void main(){\n"
+	"gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);}\n";
+
+const char* fragmentShaderSource =
+	"#version 330 core \n"
+	"out vec4 FragColor;\n"
+	"void main(){\n"
+	"FragColor = vec4(1.0f,0.5f,0.2f,1.0f);}\n";
+
 void processInput(GLFWwindow* window){
 	if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
@@ -42,6 +69,43 @@ int main(){
 	}
 
 	glViewport(0,0,800,600);
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
+	glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+
+	unsigned int VAO;
+	glGenVertexArrays(1,&VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1,&VBO);
+	glBindBuffer(GL_ARRAY_BUFFER,VBO);
+	glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW); 
+
+	unsigned int EBO;
+	glGenBuffers(1,&EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader,1,&vertexShaderSource,NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram,vertexShader);
+	glAttachShader(shaderProgram,fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+	glEnableVertexAttribArray(0);
+
 
 	//循环等待命令响应
 	while(!glfwWindowShouldClose(window))
@@ -52,6 +116,12 @@ int main(){
 		glClearColor(0.2f,0.3f,0.3f,1.0f);//设置每次屏的颜色（R,G，B，不透明度）
 		glClear(GL_COLOR_BUFFER_BIT); //清除缓存的颜色数据，每次循环开始清屏上次的颜色
 
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+		glUseProgram(shaderProgram);
+		/*glDrawArrays(GL_TRIANGLES,0,6);*/
+		glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
 
 
 		//检查新的输入和准备渲染缓存
